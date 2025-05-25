f5b79fd3c70d4798d376649cad2de92caad5f96a
Shaunett <innovationsdesigntechnology@gmail.com>
2025-05-25 05:51:12 -0400

    add new udates and images

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ hamburgerMenu.addEventListener("click", function (e) {
   document.querySelector(".sidebar").classList.toggle("change");
   hamburgerMenu.classList.toggle("change");
 });
+
+const scrollbtn = document.querySelector(".scroll-btn");
+scrollbtn.addEventListener("click", function () {
+  document.querySelector("html").style.scrollBehavior = "smooth";
+  setTimeout(function () {
+    document.querySelector("html").style.scrollBehavior = "unset";
+  }, 1000);
+});
